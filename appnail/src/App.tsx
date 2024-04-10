9eab0ce83b4de3fc768d8268a04d485cc1dbe065
jacob <jacob@ehicas.com.vn>
2024-04-10 21:54:59 +0700

    update react-redux

--- appnail/src/App.tsx
+++ appnail/src/App.tsx
@@ -1,14 +1,21 @@
+// App.js
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useAppSelector, useAppDispatch } from "./hooks/hooks";
 import { setUserValue } from "./redux/user/userSlice";
+
 function App() {
-  
+  const userValue = useAppSelector((state: any) => state.user.Value);
+  console.log(userValue);
+  const dispatch = useAppDispatch();
 
-
+  const handleClick = () => {
+    dispatch(setUserValue("New Value")); // Thay đổi giá trị của Provider
+  };
 
   return (
     <div>
-      
+      <h1>User Value: {userValue}</h1>
+      <button onClick={handleClick}>Change Value</button>
     </div>
   );
 }
